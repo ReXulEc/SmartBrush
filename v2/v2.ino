7219ed501d58d39a714bbc13eb524a6b5ec02cfb
Mert Doğu <mertdoguonder@gmail.com>
2021-10-17 15:33:37 +0300

    add: slash commands

--- v2/v2.ino
+++ v2/v2.ino
@@ -8,17 +8,50 @@
 #include <UniversalTelegramBot.h>
 
 //Wifi Settings
-const char* ssid = "SUPERONLINE_Wi-Fi_8476";
-const char* password = "S6RFKCKes7Cu";
+const char* ssid = "xxxxxxxxxxxxxxxxxxxx";
+const char* password = "xxxxxxxxx";
 
-#define botToken "1579138550:AAGnj5PIFMVOKFHHzdnM2KJX2jPnh-NvIzQ"  //Bot-Token from Botfather)
-#define userID "1002010782"
+#define botToken "xxxxxxxxxxxxxxxxxxxxxxxx"  //Bot-Token from Botfather)
+#define userID "xxxxxxxxx"
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(botToken, client);
 
 int buzzerpin = 15;
-int switchState = 0;
+int sendMessageState = 0;
+int alarmState = 0;
+
+// Slash commands
+
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
+
+unsigned long bot_lasttime; // last time messages' scan has been done
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+
+void handleNewMessages(int numNewMessages)
+{
+  
+  String answer;
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    telegramMessage &msg = bot.messages[i];
+    Serial.println("Received " + msg.text + " from " + msg.from_name);
+    if (msg.text == "/help")
+      answer = "Yardıma mı ihtiyacınız var?\nAktif olan konumlar:\n/github , /durum";
+    else if (msg.text == "/start")
+      answer = "Merhaba *" + msg.from_name + "*.\nEğer bir sorun var ise lütfen @rexulec adresine sorunu bildirin\n/help";
+    else if (msg.text == "/durum")
+      answer = "💻: ✅\n📶: ✅";
+    else if (msg.text == "/github")
+      answer = "https://github.com/rexulec/SmartBrush";
+    else
+      answer = "Ne dediğini anlayamıyorum. Lütfen /help 'i veya /yardim 'ı kullanmayı dene.";
+
+    bot.sendMessage(msg.chat_id, answer, "Markdown");
+  }
+}
+
+// Slash Commands ending
 
 void setup() {
   Serial.begin(115200);
@@ -27,6 +60,7 @@ void setup() {
   // Verwendete Pins
   pinMode(buzzerpin, OUTPUT);
   pinMode(13, INPUT); //We Set The Button's Pin
+  pinMode(12, INPUT);
 
   //Connect to WiFi
   Serial.print("Connecting to: ");
@@ -43,17 +77,46 @@ void setup() {
   Serial.println("");
   Serial.println("Bot Connected!");
 
-  bot.sendMessage(userID, "Bot Connected to the Server!", "");
+  bot.sendMessage(userID, "status:on", "");
 }
 
 void loop() {
 
-  switchState = digitalRead(13);
-  Serial.println(switchState);
-  if (switchState) {
-    bot.sendMessage(userID, "Button pressed.", "");
+  sendMessageState = digitalRead(13);
+  if (sendMessageState) {
+    Serial.println("13");
+    bot.sendMessage(userID, "Sonuç: Çocuğunuz dişini fırçaladı/fırçalamaya başladı.", "");
+  }
+
+  alarmState = digitalRead(12);
+  if (alarmState) {
+    Serial.println("12");
+    delay(10000);
+    bot.sendMessage(userID, "Uyarı: Çocuğunuzun Diş Fırçalama Vakti Geldi.", "");
     tone(buzzerpin, 1000, 250);
     delay(250);
     tone(buzzerpin, 600, 250);
+    delay(1000);
+    tone(buzzerpin, 1000, 250);
+    delay(250);
+    tone(buzzerpin, 600, 250);
+    delay(1000);
+    tone(buzzerpin, 1000, 250);
+    delay(250);
+    tone(buzzerpin, 600, 250);
+  }
+
+    if (millis() - bot_lasttime > BOT_MTBS)
+  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages)
+    {
+      Serial.println("warn: new message");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    bot_lasttime = millis();
   }
 }
